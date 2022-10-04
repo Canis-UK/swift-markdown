f8fbcc39f8984bc8a06c4cc918540c6738689256
Canis Lupus <Canis-UK@users.noreply.github.com>
2022-10-04 11:24:44 +0100

    Fix apple/swift-markdown#80 by translating $0.text.startIndex relative to untrimmedText, before passing it in to LineSegment's initialiser as parseIndex.

--- Sources/Markdown/Parser/BlockDirectiveParser.swift
+++ Sources/Markdown/Parser/BlockDirectiveParser.swift
@@ -673,7 +673,9 @@ private enum ParseContainer: CustomStringConvertible {
                                     nameLocation: pendingBlockDirective.atLocation,
                                     argumentText: DirectiveArgumentText(segments: pendingBlockDirective.argumentsText.map {
                                         let untrimmedText = String($0.text.base[$0.text.base.startIndex..<$0.text.endIndex])
-                                        return DirectiveArgumentText.LineSegment(untrimmedText: untrimmedText, lineStartIndex: untrimmedText.startIndex, parseIndex: $0.text.startIndex, range: $0.range)
+                                        let rawOffset = $0.text.base.distance(from: $0.text.base.startIndex, to: $0.text.startIndex)
+                                        let parseIndex = untrimmedText.index(untrimmedText.startIndex, offsetBy: rawOffset)
+                                        return DirectiveArgumentText.LineSegment(untrimmedText: untrimmedText, lineStartIndex: untrimmedText.startIndex, parseIndex: parseIndex, range: $0.range)
                                     }),
                                     parsedRange: pendingBlockDirective.atLocation..<pendingBlockDirective.endLocation,
                                     children)]
